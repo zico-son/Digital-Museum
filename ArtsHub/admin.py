8ac9cf54b9e424111bec42b4beef41da2eadfb8c
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-30 00:34:14 +0300

    register ArtsHubAPP models

--- ArtsHub/admin.py
+++ ArtsHub/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from ArtsHub.models import Hall, ArtObject, ArtStory, Chariot, Painting, Other, Holding, BorrowedCollection, PermenatantCollection , ArtObjectImage
 
-# Register your models here.
+class ArtStoryInline(admin.StackedInline):
+    model = ArtStory
+    extra = 1
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ArtObject)
+class ArtObjectAdmin(admin.ModelAdmin):
+    inlines = [ArtStoryInline]
+
+@admin.register(ArtStory)
+class ArtStoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Chariot)
+class ChariotAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Painting)
+class PaintingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Other)
+class OtherAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Holding)
+class HoldingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BorrowedCollection)
+class BorrowedCollectionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PermenatantCollection)
+class PermenatantCollectionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ArtObjectImage)
+class ArtObjectImageAdmin(admin.ModelAdmin):
+    pass
+
