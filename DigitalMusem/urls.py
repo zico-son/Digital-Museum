3db578a49a3be1c74a7dbe68d1319735fb086918
saharyasser388 <saharyasser388@gmail.com>
2023-04-29 17:18:03 +0200

    pass master app urls to mainapp urls

--- DigitalMusem/urls.py
+++ DigitalMusem/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('arts/', include(('ArtsHub.urls', 'ArtsHub'))),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('', include('MuseumMaster.urls')),
 ]
