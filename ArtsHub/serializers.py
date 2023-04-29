fb847886d87d073ad6802716ec6c7e30ca676ef8
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-30 00:33:57 +0300

    add ArtsHubAPP serializers

---
+++ ArtsHub/serializers.py
@@ -0,0 +1,105 @@
+from rest_framework import serializers
+from ArtsHub.models import Hall, ArtObject, ArtStory, Chariot, Painting, Other, Holding, BorrowedCollection, PermenatantCollection, ArtObjectImage
+
+
+class HallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hall
+        fields = ['id', 'name']
+
+class ArtStorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArtStory
+        fields = ['title', 'description']
+
+class ChariotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chariot
+        fields = ['object_number', 'origin','chassis_number']
+
+class PaintingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Painting
+        fields = ['artist_name']
+
+class OtherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Other
+        fields = ['origin']
+
+class HoldingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Holding
+        fields = ['matrial']
+
+class BorrowedCollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BorrowedCollection
+        fields = ['date_of_borrowing', 'date_of_return']
+
+class PermenatantCollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PermenatantCollection
+        fields = ['date_of_acquisition']
+
+class ArtObjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArtObjectImage
+        fields = ['image']
+
+class ArtObjectSerializer(serializers.ModelSerializer):
+    hall = HallSerializer(read_only=True)
+    art_story = ArtStorySerializer(read_only=True)
+    images = ArtObjectImageSerializer(many=True, read_only=True)
+    type = serializers.SerializerMethodField()
+    content = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    status_info = serializers.SerializerMethodField()
+    class Meta:
+        model = ArtObject
+        fields = ['id', 'name', 'epoch', 'description', 'hall','images', 'art_story', 'created_at', 'updated_at','active', 'highlighted', 'type','content' ,'status', 'status_info']
+
+    def get_type(self, obj):
+        try:
+            if obj.chariot:
+                return 'chariot'
+        except:
+            try:
+                if obj.painting:
+                    return 'painting'
+            except:
+                try:
+                    if obj.other:
+                        return 'other'
+                except:
+                    return 'holdings'
+
+
+    def get_content(self, obj):
+        try:
+            if obj.chariot:
+                return ChariotSerializer(obj.chariot).data
+        except:
+            try:
+                if obj.painting:
+                    return PaintingSerializer(obj.painting).data
+            except:
+                try:
+                    if obj.other:
+                        return OtherSerializer(obj.other).data
+                except:
+                    return HoldingSerializer(obj.holdings, many = True).data
+
+    def get_status(self, obj):
+        try:
+            if obj.borrowed_collection:
+                return 'borrowed'
+        except:
+            return 'permenatant'
+
+    def get_status_info(self, obj):
+        try:
+            if obj.borrowed_collection:
+                return BorrowedCollectionSerializer(obj.borrowed_collection).data
+        except:
+            return PermenatantCollectionSerializer(obj.permenatant_collection).data
