fdfd6b1bc1631a67f3219ab120dc5c39b28258f5
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-29 23:41:46 +0300

    add media and static URL and ROOT

--- DigitalMusem/urls.py
+++ DigitalMusem/urls.py
@@ -20,7 +20,7 @@ from django.conf  import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('ArtsHub.urls', 'ArtsHub'), namespace='ArtsHub')),
+    path('', include(('ArtsHub.urls', 'ArtsHub'))),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 if settings.DEBUG:
