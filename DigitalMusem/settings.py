e1e29d42b996fe0374fac046560e4155ac95a386
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-01 23:08:20 +0300

    Initial Deployment

--- DigitalMusem/settings.py
+++ DigitalMusem/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-1dd0tw0(z$4+*w**!kn!p^^azaeas2fiasr=^rc(jckg+ztz2z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     "phonenumber_field",
     "debug_toolbar",
     'rest_framework',
@@ -48,8 +50,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
