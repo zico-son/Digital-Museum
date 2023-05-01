ba62164a9ecd8aa54897543c9641df1750899872
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-01 20:53:52 +0300

    add Media urls

--- DigitalMusem/urls.py
+++ DigitalMusem/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static  import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('arts/', include(('ArtsHub.urls', 'ArtsHub'))),
     path('__debug__/', include('debug_toolbar.urls')),
     path('', include('MuseumMaster.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
