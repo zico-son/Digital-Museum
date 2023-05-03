9b0b64a66c094e55c37e98e032a51c4f6a127b74
saharyasser388 <saharyasser388@gmail.com>
2023-05-03 23:39:55 +0200

    add limitations for adding museum info only once

--- MuseumMaster/admin.py
+++ MuseumMaster/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from .models import *
 from . import models
 from . import*
 # Register your models here.
+class MuseumInfoAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if MuseumInfo.objects.count() == 1:
+            return False
+        return True
+        
 admin.site.register(models.Event)
 admin.site.register(models.Media)
-admin.site.register(models.MuseumInfo)
+admin.site.register(models.MuseumInfo,MuseumInfoAdmin)
 admin.site.register(models.OpenningHour)
