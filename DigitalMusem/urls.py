16370a4d08749a80ed82613e7a006c026e22b11d
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-05 23:18:06 +0300

    Update admin information

--- DigitalMusem/urls.py
+++ DigitalMusem/urls.py
@@ -26,3 +26,9 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+admin.site.site_header = "Digital Museum Admin"
+admin.site.site_title = "Digital Museum Admin Portal"
+admin.site.index_title = "Welcome to Digital Museum Portal"
