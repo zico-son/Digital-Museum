e7361393ea4b2eeed7686074bb87a9f76f0186f3
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-06 01:01:46 +0300

    add halls endpoint

--- ArtsHub/urls.py
+++ ArtsHub/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from ArtsHub.views import ArtObjectViewSet
+from ArtsHub.views import ArtObjectViewSet, HallViewSet
 router = DefaultRouter()
 router.register('artobjects', ArtObjectViewSet, basename='artobjects')
+router.register('halls', HallViewSet, basename='halls')
 urlpatterns = router.urls
