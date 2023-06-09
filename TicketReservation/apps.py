11df71af97e2383bec999dd15b68ae83a8375d76
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2023-06-09 22:58:25 +0300

    edit class name in apps

--- TicketReservation/apps.py
+++ TicketReservation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TicketreservaionConfig(AppConfig):
+class TicketreservtaionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TicketReservaion'
+    name = 'TicketReservation'
