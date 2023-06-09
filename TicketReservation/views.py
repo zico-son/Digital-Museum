8413a86c5c3eade41c56a443f79f1bb88e51d3b8
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2023-06-09 22:58:51 +0300

    add reservation views

--- TicketReservation/views.py
+++ TicketReservation/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import *
+from .models import *
 
 # Create your views here.
+
+
+
+class TicketViewSet(ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+class OrderItemViewSet(ModelViewSet):
+    queryset = OrderItem.objects.select_related('ticket').all()
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ViewOrderItemSerializer
+        return OrderItemSerializer
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.prefetch_related('tickets__ticket').all()
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ViewOrderSerializer
+        return OrderSerializer
