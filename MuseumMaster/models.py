b565a2316806a08cc7fa470b651571800a649668
saharyasser388 <saharyasser388@gmail.com>
2023-06-11 23:26:03 +0300

    show more details in event admin

--- MuseumMaster/models.py
+++ MuseumMaster/models.py
@@ -19,13 +19,13 @@ class MuseumInfo(models.Model):
     
 class OpenningHour(models.Model):
     DAYS_OF_WEEK = (
+    ('Saturday', 'Saturday'),
+    ('Sunday', 'Sunday'),    
     ('Monday', 'Monday'),
     ('Tuesday', 'Tuesday'),
     ('Wednesday', 'Wednesday'),
     ('Thursday', 'Thursday'),
     ('Friday', 'Friday'),
-    ('Saturday', 'Saturday'),
-    ('Sunday', 'Sunday'),
     )
 
     day = models.CharField(max_length=20, choices=DAYS_OF_WEEK,null=True,blank=True)
